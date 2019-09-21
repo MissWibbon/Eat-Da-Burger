9ca330c0436e24a2e3d3f34d1bd225a4ab2ca0d4
Michelle <wuebben.michelle@yahoo.com>
2019-09-21 15:32:50 -0500

    updated submit value

--- server.js
+++ server.js
@@ -15,74 +15,11 @@ app.use(express.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "day_planner_db"
-});
+var routes = require("./controllers/burgers_controller.js");
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-
-  console.log("connected as id " + connection.threadId);
-});
-
-// Root get route
-app.get("/", function(req, res) {
-  connection.query("SELECT * FROM plans;", function(err, data) {
-    if (err) throw err;
-
-    // Test it
-    // console.log('The solution is: ', data);
-
-    // Test it
-    // return res.send(data);
-
-    res.render("index", { plans: data });
-  });
-});
-
-// Post route -> back to home
-app.post("/", function(req, res) {
-  // Test it
-   console.log('You sent, ' + req.body.plan);
-
-  // Test it
-   //return res.send('You sent, ' + req.body.wish);
-
-  // When using the MySQL package, we'd use ?s in place of any values to be inserted, which are then swapped out with corresponding elements in the array
-  // This helps us avoid an exploit known as SQL injection which we'd be open to if we used string concatenation
-  // https://en.wikipedia.org/wiki/SQL_injection
-  connection.query("INSERT INTO plans (plan) VALUES (?)", [req.body.plan], function(err, result) {
-    if (err) throw err;
-
-    res.redirect("/");
-  });
-});
+app.use(routes);
 
 
-// Delete route -> back to home
-app.delete("/", function(req, res) {
-  // Test it
-   console.log('You deleted, ' + req.body.plan);
-
-  // Test it
-   //return res.send('You sent, ' + req.body.wish);
-
-  // When using the MySQL package, we'd use ?s in place of any values to be inserted, which are then swapped out with corresponding elements in the array
-  // This helps us avoid an exploit known as SQL injection which we'd be open to if we used string concatenation
-  // https://en.wikipedia.org/wiki/SQL_injection
-  connection.query("DELETE plans Where (plan) VALUES (?)", [req.body.plan], function(err, result) {
-    if (err) throw err;
-
-    res.redirect("/");
-  });
-});
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
